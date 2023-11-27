6c967ec49e2b677689bfb35516bf6d60b9a784ab
ppliatsik <ppliatsik@gmail.com>
2023-11-27 11:03:05 +0200

    CRLF to CR.

--- dock/db-init/create-test-db.sh
+++ dock/db-init/create-test-db.sh
@@ -1,16 +1,1 @@
-#!/bin/bash
-
-# taken from:
-# https://github.com/docker-library/postgres/issues/151
-
-set -e
-
-POSTGRES="psql --username ${POSTGRES_USER} -d ${POSTGRES_DB}"
-
-echo "Creating database: ${POSTGRES_DB}"
-
-$POSTGRES <<EOSQL
-CREATE DATABASE ${POSTGRES_DB} OWNER ${POSTGRES_USER};
-\connect ${POSTGRES_DB};
-CREATE EXTENSION postgis;
-EOSQL
+#!/bin/bash# taken from:# https://github.com/docker-library/postgres/issues/151set -ePOSTGRES="psql --username ${POSTGRES_USER} -d ${POSTGRES_DB}"echo "Creating database: ${POSTGRES_DB}"$POSTGRES <<EOSQLCREATE DATABASE ${POSTGRES_DB} OWNER ${POSTGRES_USER};\connect ${POSTGRES_DB};CREATE EXTENSION postgis;EOSQL
